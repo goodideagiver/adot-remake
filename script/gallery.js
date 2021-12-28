bdd7ebe96d4a97e648bc34c0aa9a975deeb505fe
prplblck <kariurek@gmail.com>
2021-12-28 22:29:25 +0100

    js begin

---
+++ script/gallery.js
@@ -0,0 +1,11 @@
+const gallimg = document.querySelectorAll('.images img');
+console.log(gallimg);
+
+gallimg.forEach(galimage => {
+    galimage.addEventListener('click',() => galleryenabler(galimage));
+});
+
+function galleryenabler(clickede) {
+    console.log('Clicked' + clickede)
+    clickede.classList.toggle("enabled");
+}

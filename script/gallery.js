9b95736421bac3edd2941167b46293a0aab12b20
prplblck <kariurek@gmail.com>
2021-12-29 22:15:13 +0100

    lightbox js start

--- script/gallery.js
+++ script/gallery.js
@@ -1,21 +1,28 @@
+// function galleryenabler(clickede) {
+//     console.log('Clicked' + clickede)
+//     if (clickede.classList.contains("enabled")) {
+//         clickede.classList.remove("enabled");
+//     } else {
+//         gallimg.forEach(element => {
+//             element.classList.remove("enabled");
+//         });
+//         clickede.classList.add("enabled");
+//         setTimeout(() => {
+//             clickede.scrollIntoView({block: "center",behavior: "smooth"});
+//         }, 500);
+//     }
+// }
+
 const gallimg = document.querySelectorAll('.images img');
-//console.log(gallimg);
+gallContainer = document.createElement("div");
+gallContainer.classList.add("lightbox");
+gallimg.forEach(element => {
+    gallContainer.appendChild(element.cloneNode(element));
+    //console.log(element);
+});
+//console.log(gallContainer)
 
 gallimg.forEach(galimage => {
-    galimage.addEventListener('click',() => galleryenabler(galimage));
+    galimage.addEventListener('click',() => showGallery(galimage));
 });
 
-function galleryenabler(clickede) {
-    console.log('Clicked' + clickede)
-    if (clickede.classList.contains("enabled")) {
-        clickede.classList.remove("enabled");
-    } else {
-        gallimg.forEach(element => {
-            element.classList.remove("enabled");
-        });
-        clickede.classList.add("enabled");
-        setTimeout(() => {
-            clickede.scrollIntoView({block: "center",behavior: "smooth"});
-        }, 500);
-    }
-}

4b4575473d21e52fa8a7dbaa0e6f3fc5ed0f123b
Karol <kariurek@gmail.com>
2021-12-29 15:33:04 +0100

    adjust gallery and scroll rev gallery

--- script/scroll.js
+++ script/scroll.js
@@ -1,2 +1,2 @@
-ScrollReveal().reveal('.images img',{interval:10,distance:'50px',duration:1000,delay:200});
+ScrollReveal().reveal('.images img',{interval:10,duration:1000,delay:200});
 ScrollReveal().reveal('.pricetable div>*',{interval:10,distance:'10px',duration:1000,scale:0.5});

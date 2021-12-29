6b11c16f49b8822718234f64f3054be40df7b445
prplblck <kariurek@gmail.com>
2021-12-29 20:24:46 +0100

    mobile nav

---
+++ script/nav.js
@@ -0,0 +1,12 @@
+const mainNav = document.querySelector(".main-nav");
+const mainNavHeight = mainNav.offsetHeight;
+const mobileNav = document.querySelector(".mobile-nav");
+// mobileNav.style.transform = `translate(0,${mainNavHeight-3}px)`;
+//console.log(mainNavHeight);
+const hamburger = document.querySelector("#hamburger");
+hamburger.addEventListener("click",mobileMenuToggle)
+
+function mobileMenuToggle() {
+    mobileNav.classList.toggle("list-toggle");
+}
+

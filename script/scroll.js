1cabcd1ded020a4e0c4ac10e21d61c5c02cbfe2c
Karol <kariurek@gmail.com>
2021-12-30 13:15:50 +0100

    scroll for design

--- script/scroll.js
+++ script/scroll.js
@@ -1,2 +1,3 @@
 ScrollReveal().reveal('.images img',{interval:10,duration:1000,delay:200});
 ScrollReveal().reveal('.pricetable div>*',{interval:1,distance:'10px',duration:800,scale:0.9});
+ScrollReveal().reveal('.design ol li',{interval:100,duration:1000,delay:200,distance:'10px'});

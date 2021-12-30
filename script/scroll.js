4da2a5dfedd66cf7ccaaf003349460db540e59c1
Karol <kariurek@gmail.com>
2021-12-30 14:05:03 +0100

    prettier

--- script/scroll.js
+++ script/scroll.js
@@ -1,3 +1,17 @@
-ScrollReveal().reveal('.images img',{interval:10,duration:1000,delay:200});
-ScrollReveal().reveal('.pricetable div>*',{interval:1,distance:'10px',duration:800,scale:0.9});
-ScrollReveal().reveal('.design ol li',{interval:100,duration:1000,delay:200,distance:'10px'});
+ScrollReveal().reveal(".images img", {
+	interval: 10,
+	duration: 1000,
+	delay: 200,
+});
+ScrollReveal().reveal(".pricetable div>*", {
+	interval: 1,
+	distance: "10px",
+	duration: 800,
+	scale: 0.9,
+});
+ScrollReveal().reveal(".design ol li", {
+	interval: 100,
+	duration: 1000,
+	delay: 200,
+	distance: "10px",
+});

a923acdd4b4ed69e46b2f2cfa0d08fca0eb10866
prplblck <kariurek@gmail.com>
2022-01-01 10:21:28 +0100

    gallery.js start

--- script/gallery.js
+++ script/gallery.js
@@ -16,12 +16,25 @@
 const gallimg = document.querySelectorAll(".images img");
 gallContainer = document.createElement("div");
 gallContainer.classList.add("lightbox");
+xButton = document.createElement("div").innerHTML='<i class="fas fa-times"></i>';
+gallContainer.appendChild.xButton;
 gallimg.forEach((element) => {
 	gallContainer.appendChild(element.cloneNode(true));
-	//console.log(element);
 });
-//console.log(gallContainer)
-
+document.querySelector("body > main").appendChild(gallContainer);
+gallContainer.addEventListener("click",galClose);
 gallimg.forEach((galimage) => {
 	galimage.addEventListener("click", () => showGallery(galimage));
 });
+
+
+
+
+
+function showGallery(image) {
+	galClose();
+}
+
+function galClose() {
+	gallContainer.classList.toggle('closed');
+}

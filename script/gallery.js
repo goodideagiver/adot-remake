6f12e23f3f25f7532f919fe624a994405e82b4d9
prplblck <kariurek@gmail.com>
2022-01-01 11:40:47 +0100

    working lightbox forward and backward

--- script/gallery.js
+++ script/gallery.js
@@ -16,25 +16,66 @@
 const gallimg = document.querySelectorAll(".images img");
 gallContainer = document.createElement("div");
 gallContainer.classList.add("lightbox");
-xButton = document.createElement("div").innerHTML='<i class="fas fa-times"></i>';
-gallContainer.appendChild.xButton;
+xButton = document.createElement("div");
+gallContainer.appendChild(xButton);
 gallimg.forEach((element) => {
 	gallContainer.appendChild(element.cloneNode(true));
 });
 document.querySelector("body > main").appendChild(gallContainer);
-gallContainer.addEventListener("click",galClose);
 gallimg.forEach((galimage) => {
 	galimage.addEventListener("click", () => showGallery(galimage));
 });
-
+document.querySelector(".lightbox > div").innerHTML='<i id="gallX" class="fas fa-times"></i>';
+gallContainer.innerHTML += '<div class="lightbox-controls"><i id="goLeft" class="fas fa-arrow-left"></i><i id="goRight" class="fas fa-arrow-right"></i>';
+document.getElementById("gallX").addEventListener('click',galClose);
+document.querySelector('.lightbox img').classList.add('show-img')
 
 
 
 
 function showGallery(image) {
-	galClose();
+	//document.querySelector(`.lightbox ${image}`).classList.add('show-img');
+	gallContainer.classList.toggle('closed');
+	document.querySelector('.lightbox img').classList.add('show-img')
 }
 
 function galClose() {
 	gallContainer.classList.toggle('closed');
+}
+
+const arrowLeft = document.querySelector("#goLeft");
+const arrowRight = document.querySelector("#goRight");
+let slides = document.querySelectorAll(".lightbox img")
+arrowRight.addEventListener("click",nextSlide);
+arrowLeft.addEventListener("click",prevSlide);
+
+
+function nextSlide() {
+	let active = getActiveSlideIndex();
+	slides[active].classList.toggle('show-img');
+	if (active != slides.length-1) {
+	slides[active+1].classList.toggle('show-img');
+	} else {
+		slides[0].classList.toggle('show-img');
+	}
+}
+
+function prevSlide() {
+	let active = getActiveSlideIndex();
+	slides[active].classList.toggle('show-img');
+	if (slides[active-1] == undefined) {
+		slides[slides.length-1].classList.toggle('show-img');
+	} else {
+		slides[active-1].classList.toggle('show-img');
+	}
+}
+
+function getActiveSlideIndex() {
+	let activeSlide = "";
+	for (let i = 0; i < slides.length; i++) {
+		if (slides[i].className != "") {
+			activeSlide = i;
+		}
+	}
+	return activeSlide;
 }

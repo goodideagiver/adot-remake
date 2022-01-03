c5783f89de248a85e507eb64a77afb2864c96d3b
Karol <kariurek@gmail.com>
2022-01-03 15:43:01 +0100

    working pricetable preview lightbox

--- script/pricetable.js
+++ script/pricetable.js
@@ -11,16 +11,17 @@ pricetableContainer.innerHTML = `
         ${copyPricetableInfo()}
     </main>
     <div class="lightbox-controls">
-        <div>
-            <i id="priceLeft"  class="fas fa-arrow-left"></i>
+        <div id="priceLeft">
+            <i   class="fas fa-arrow-left"></i>
         </div>
-        <div>
-            <i id="priceRight"  class="fas fa-arrow-right"></i>
+        <div id="priceRight">
+            <i   class="fas fa-arrow-right"></i>
         </div>
     </div>
 `;
 document.querySelector('body > main').appendChild(pricetableContainer);
 let paragraphs = document.querySelectorAll('.pricePreviewContainer p');
+
 paragraphs.forEach(paragraph => {
     paragraph.remove();
 });
@@ -33,11 +34,41 @@ function copyPricetableInfo() {
     });
     return infoPricetable.innerHTML;
 }
+const priceDivs = document.querySelectorAll('.pricePreviewContainer main > div');
+document.getElementById('priceX').addEventListener('click',closePricetablePreview);
+document.getElementById('priceLeft').addEventListener('click',prevPricetableItem);
+document.getElementById('priceRight').addEventListener('click',nextPricetableItem);
 
+function nextPricetableItem() {
+    let active = getActivePricetableDiv();
+    priceDivs[active].classList.remove('priceDivVisible');
+    active++;
+    if (active>priceDivs.length-1) active=0;
+    priceDivs[active].classList.add('priceDivVisible');
+}
+
+function prevPricetableItem() {
+    let active = getActivePricetableDiv();
+    priceDivs[active].classList.remove('priceDivVisible');
+    active=active-1;
+    if (active<0) active=priceDivs.length-1;
+    priceDivs[active].classList.add('priceDivVisible');
+}
+
+function closePricetablePreview() {
+    priceDivs[getActivePricetableDiv()].classList.remove('priceDivVisible');
+    pricetableContainer.classList.add('closed');
+}
+
+function getActivePricetableDiv() {
+    return getArrayItemIndex(priceDivs,document.querySelector('.priceDivVisible'));
+}
 
 function showPricetablePreview(clickedDiv) {
-    console.log(getArrayItemIndex(pricetableItems,clickedDiv.currentTarget));
-    pricetableContainer.classList.toggle('closed');
+    // console.log(getArrayItemIndex(pricetableItems,clickedDiv.currentTarget));
+    let clickedDiv1 = getArrayItemIndex(pricetableItems,clickedDiv.currentTarget);
+    pricetableContainer.classList.remove('closed');
+    priceDivs[clickedDiv1].classList.add('priceDivVisible');
 }
 
 

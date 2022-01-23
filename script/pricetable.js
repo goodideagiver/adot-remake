145e146b5015271849bfa0172e03efbd285b31b9
prplblck <kariurek@gmail.com>
2022-01-23 18:18:08 +0100

    close pricetable on bg clock

--- script/pricetable.js
+++ script/pricetable.js
@@ -1,6 +1,6 @@
 const pricetableItems = document.querySelectorAll('.pricetable > div');
-pricetableContainer = document.createElement("div");
-pricetableContainer.classList.add('pricePreviewContainer','closed');
+pricetableContainer = document.createElement('div');
+pricetableContainer.classList.add('pricePreviewContainer', 'closed');
 pricetableContainer.innerHTML = `
 <div class="lightbox-controls">
     <div id="priceX">
@@ -20,54 +20,73 @@ pricetableContainer.innerHTML = `
     </div>
 `;
 document.querySelector('body > main').appendChild(pricetableContainer);
+pricetableContainer.addEventListener('click', () => {
+	if (event.target === pricetableContainer) {
+		closePricetablePreview();
+	}
+});
 
 function copyPricetableInfo() {
-    infoPricetable = document.createElement("main");
-    pricetableItems.forEach(priceDiv => {
-        infoPricetable.appendChild(priceDiv.cloneNode(true));
-        priceDiv.addEventListener('click',showPricetablePreview);
-    });
-    infoPricetable.querySelectorAll('p').forEach(element => {
-        element.remove();
-    });
-    return infoPricetable.innerHTML;
+	infoPricetable = document.createElement('main');
+	pricetableItems.forEach((priceDiv) => {
+		infoPricetable.appendChild(priceDiv.cloneNode(true));
+		priceDiv.addEventListener('click', showPricetablePreview);
+	});
+	infoPricetable.querySelectorAll('p').forEach((element) => {
+		element.remove();
+	});
+	return infoPricetable.innerHTML;
 }
-const priceDivs = document.querySelectorAll('.pricePreviewContainer main > div');
-document.getElementById('priceX').addEventListener('click',closePricetablePreview);
-document.getElementById('priceLeft').addEventListener('click',prevPricetableItem);
-document.getElementById('priceRight').addEventListener('click',nextPricetableItem);
+const priceDivs = document.querySelectorAll(
+	'.pricePreviewContainer main > div'
+);
+document
+	.getElementById('priceX')
+	.addEventListener('click', closePricetablePreview);
+document
+	.getElementById('priceLeft')
+	.addEventListener('click', prevPricetableItem);
+document
+	.getElementById('priceRight')
+	.addEventListener('click', nextPricetableItem);
 
 function nextPricetableItem() {
-    let active = getActivePricetableDiv();
-    priceDivs[active].classList.remove('priceDivVisible');
-    active++;
-    if (active>priceDivs.length-1) active=0;
-    priceDivs[active].classList.add('priceDivVisible');
+	let active = getActivePricetableDiv();
+	priceDivs[active].classList.remove('priceDivVisible');
+	active++;
+	if (active > priceDivs.length - 1) active = 0;
+	priceDivs[active].classList.add('priceDivVisible');
 }
 
 function prevPricetableItem() {
-    let active = getActivePricetableDiv();
-    priceDivs[active].classList.remove('priceDivVisible');
-    active=active-1;
-    if (active<0) active=priceDivs.length-1;
-    priceDivs[active].classList.add('priceDivVisible');
+	let active = getActivePricetableDiv();
+	priceDivs[active].classList.remove('priceDivVisible');
+	active = active - 1;
+	if (active < 0) active = priceDivs.length - 1;
+	priceDivs[active].classList.add('priceDivVisible');
 }
 
 function closePricetablePreview() {
-    toggleBodyScroll("on");
-    let active = getActivePricetableDiv();
-    priceDivs[active].classList.remove('priceDivVisible');
-    pricetableContainer.classList.add('closed');
-    pricetableItems[active].scrollIntoView({block:"center"});
+	toggleBodyScroll('on');
+	let active = getActivePricetableDiv();
+	priceDivs[active].classList.remove('priceDivVisible');
+	pricetableContainer.classList.add('closed');
+	pricetableItems[active].scrollIntoView({ block: 'center' });
 }
 
 function getActivePricetableDiv() {
-    return getArrayItemIndex(priceDivs,document.querySelector('.priceDivVisible'));
+	return getArrayItemIndex(
+		priceDivs,
+		document.querySelector('.priceDivVisible')
+	);
 }
 
 function showPricetablePreview(clickedDiv) {
-    toggleBodyScroll("off");
-    let clickedDiv1 = getArrayItemIndex(pricetableItems,clickedDiv.currentTarget);
-    pricetableContainer.classList.remove('closed');
-    priceDivs[clickedDiv1].classList.add('priceDivVisible');
+	toggleBodyScroll('off');
+	let clickedDiv1 = getArrayItemIndex(
+		pricetableItems,
+		clickedDiv.currentTarget
+	);
+	pricetableContainer.classList.remove('closed');
+	priceDivs[clickedDiv1].classList.add('priceDivVisible');
 }

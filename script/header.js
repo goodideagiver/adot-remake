591b5b829adaab72e695811f72b123e254e300e5
prplblck <kariurek@gmail.com>
2022-03-12 09:06:14 +0100

    added animated slider to header, fixed image not high enough in about section

--- script/header.js
+++ script/header.js
@@ -1,21 +1,31 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+const headerContainer = document.querySelector('.main-h main');
+const images = [...document.querySelectorAll('.images img')];
+
+const galleryContainer = document.createElement('div');
+galleryContainer.classList = 'header-gallery';
+
+const imageList = images.map((image, index) => {
+	const imageWrapper = document.createElement('div');
+	imageWrapper.classList = 'image-header-container';
+	imageWrapper.append(image.cloneNode(true));
+	galleryContainer.appendChild(imageWrapper);
+	if (index !== 0) imageWrapper.classList.add('image-header-hide');
+	return imageWrapper;
+});
+
+headerContainer.append(galleryContainer);
+
+const nextSlideHeader = () => {
+	const activeSlide = imageList.find(
+		image => !image.classList.contains('image-header-hide')
+	);
+	const activeSlideIndex = imageList.indexOf(activeSlide);
+	const nextSlideIndex =
+		activeSlideIndex === imageList.length - 1 ? 0 : activeSlideIndex + 1;
+	imageList[activeSlideIndex].classList.add('image-header-hide');
+	imageList[nextSlideIndex].classList.remove('image-header-hide');
+};
+
+setInterval(() => {
+	nextSlideHeader();
+}, 10000);
